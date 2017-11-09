747a0120be5e751f713e90fc1df8bdf1cf161263
vendi <svitlana.vakulenko@gmail.com>
2017-11-09 09:58:19 +0100

    fix: import index name

--- setup_index.py
+++ setup_index.py
@@ -4,12 +4,17 @@ svakulenko
 9 Nov 2017
 '''
 from elasticsearch import Elasticsearch
+from all_settings import INDEX
 
-es = Elasticsearch()
+def create_index(index_name):
+    es = Elasticsearch()
 
-# reset index
-try:
-    es.indices.delete(index=index_name)
-    es.indices.create(index=index_name, body=mapping)
-except Exception as e:
-    print (e)
+    # reset index
+    try:
+        es.indices.delete(index=index_name)
+        es.indices.create(index=index_name, body=mapping)
+    except Exception as e:
+        print (e)
+
+if __name__ == '__main__':
+    create_index(INDEX)
