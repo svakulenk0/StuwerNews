e79fd14bd681845bbc76f3a40c6a1c913b3aa782
vendi <svitlana.vakulenko@gmail.com>
2017-11-09 10:04:14 +0100

    fix: hide credentials placeholders

--- all_settings.py
+++ all_settings.py
@@ -15,18 +15,18 @@ KEYWORDS = ['praterstern', 'leopoldstadt']
 INDEX = 'communidata'
 
 # Twitter API credentials
-APP_KEY = 'TODO'
-APP_SECRET = 'TODO'
-OAUTH_TOKEN = 'TODO'
-OAUTH_TOKEN_SECRET = 'TODO'
+# APP_KEY = 'TODO'
+# APP_SECRET = 'TODO'
+# OAUTH_TOKEN = 'TODO'
+# OAUTH_TOKEN_SECRET = 'TODO'
 
-# Twitter user name
-MY_NAME = 'StuwerNews'
+# # Twitter user name
+# MY_NAME = 'StuwerNews'
 
-# Babelfy API credentials
-BABELFY_KEY = 'TODO'
+# # Babelfy API credentials
+# BABELFY_KEY = 'TODO'
 
-# Google Custom Search API
-GOOGLE_API_KEY = 'TODO'
-# search Austrian german-language newspapers
-GOOGLE_CUSTOM_SE = 'TODO'
+# # Google Custom Search API
+# GOOGLE_API_KEY = 'TODO'
+# # search Austrian german-language newspapers
+# GOOGLE_CUSTOM_SE = 'TODO'
